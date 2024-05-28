b18efffee8f3ddd329df9d48a01a8aa0b5f3e661
hugoarmor <hugoarmoreira@hotmail.com>
2024-05-28 20:29:54 -0300

    update: config

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -60,7 +60,7 @@ return {
           ["<CR>"] = cmp.mapping.confirm({ select = true }),
         }),
         sources = cmp.config.sources({
-          { name = "nvim_lsp" },
+          { name = "nvim_lsp", priority = 1000 },
           { name = "luasnip" }, -- For luasnip users.
         }, {
           { name = "buffer" },
