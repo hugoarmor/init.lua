a6f6595d9044531e9daf9f20d84b9794403223cd
hugoarmor <hugoarmoreira@hotmail.com>
2024-06-14 12:25:43 -0300

    update: config

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -19,31 +19,31 @@ return {
       require("luasnip.loaders.from_vscode").lazy_load()
 
       local cmp_kinds = {
-        Text = '  ',
-        Method = '  ',
-        Function = '  ',
-        Constructor = '  ',
-        Field = '  ',
-        Variable = '  ',
-        Class = '  ',
-        Interface = '  ',
-        Module = '  ',
-        Property = '  ',
-        Unit = '  ',
-        Value = '  ',
-        Enum = '  ',
-        Keyword = '  ',
-        Snippet = '  ',
-        Color = '  ',
-        File = '  ',
-        Reference = '  ',
-        Folder = '  ',
-        EnumMember = '  ',
-        Constant = '  ',
-        Struct = '  ',
-        Event = '  ',
-        Operator = '  ',
-        TypeParameter = '  ',
+        Text = "  ",
+        Method = "  ",
+        Function = "  ",
+        Constructor = "  ",
+        Field = "  ",
+        Variable = "  ",
+        Class = "  ",
+        Interface = "  ",
+        Module = "  ",
+        Property = "  ",
+        Unit = "  ",
+        Value = "  ",
+        Enum = "  ",
+        Keyword = "  ",
+        Snippet = "  ",
+        Color = "  ",
+        File = "  ",
+        Reference = "  ",
+        Folder = "  ",
+        EnumMember = "  ",
+        Constant = "  ",
+        Struct = "  ",
+        Event = "  ",
+        Operator = "  ",
+        TypeParameter = "  ",
       }
 
       cmp.setup({
@@ -53,11 +53,33 @@ return {
           end,
         },
         mapping = cmp.mapping.preset.insert({
-          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-j>"] = cmp.mapping.select_next_item(),
+          ["<C-k>"] = cmp.mapping.select_prev_item(),
+          ["<C-d>"] = cmp.mapping.scroll_docs(-4),
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
-          ["<C-Space>"] = cmp.mapping.complete(),
-          ["<C-e>"] = cmp.mapping.abort(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<C-Space>"] = cmp.mapping.complete({}),
+          ["<CR>"] = cmp.mapping.confirm({
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
+          }),
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_locally_jumpable() then
+              luasnip.expand_or_jump()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.locally_jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
         }),
         sources = cmp.config.sources({
           { name = "nvim_lsp", priority = 1000 },
@@ -76,7 +98,6 @@ return {
             return vim_item
           end,
         },
-        cmp
       })
     end,
   },
